9192ab274e3460efbd3c18a752c28c9104b36fe1
M3NGSZE <mengse.thun13@gmail.com>
2025-07-18 12:32:38 +0700

    client and server component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <main>
       <h1>hello world</h1>
       <Link href="/users">UserPage</Link>
+      
+      <ProductCard/>
     </main>
   );
 }
