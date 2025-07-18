3f92ccf751946107c6c725000f73853e55a236c8
M3NGSZE <mengse.thun13@gmail.com>
2025-07-18 13:51:08 +0700

    add comment to revalidation

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -9,7 +9,7 @@ const UserPage = async () => {
 
     // const res = await fetch("https://jsonplaceholder.typicode.com/users", {cache: 'no-store'}); use to disable caching
 
-    const res = await fetch("https://jsonplaceholder.typicode.com/users", // keep data fresh for a certain period of time
+    const res = await fetch("https://jsonplaceholder.typicode.com/users", // keep data fresh for a certain period of time // if use third party library we will no have the cach
         { next: { revalidate : 10} });
 
     const users: User[] = await res.json();
