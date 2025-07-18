d878fc5e0d279791d18506a796cd8c47355b01ec
M3NGSZE <mengse.thun13@gmail.com>
2025-07-18 21:40:14 +0700

    tailwind

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import AddToCart from './AddToCart'
-import styles from './ProductCart.module.css'
+// import styles from './ProductCart.module.css'
 
 const ProductCard = () => {
     return (
-        <div className={styles.card}>
+        <div /*className={styles.card}*/ className='p-5 my-5 bg-sky-400 text-xl hover:bg-sky-500'>
             <AddToCart/>
         </div>
     )
