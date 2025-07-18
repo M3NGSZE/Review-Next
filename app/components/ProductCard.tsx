c7669c258e793dc2d9f2d9dcd31f32b1f7c73f77
M3NGSZE <mengse.thun13@gmail.com>
2025-07-18 22:10:18 +0700

    daisy ui

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -4,10 +4,11 @@ import AddToCart from './AddToCart'
 
 const ProductCard = () => {
     return (
-        <div /*className={styles.card}*/ className='p-5 my-5 bg-sky-400 text-xl hover:bg-sky-500'>
-            <AddToCart/>
+        <div /*className={styles.card} className='p-5 my-5 bg-sky-400 text-xl hover:bg-sky-500'*/
+        >
+            <AddToCart />
         </div>
-    )
+    );
 }
 
 export default ProductCard
